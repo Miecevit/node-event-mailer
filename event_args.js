f06a2670e8a28f541688a010bb331639c7361dcc
Miecevit <mert.iecevit@gmail.com>
2023-03-17 10:12:16 +0300

    Delete event_args.js

--- event_args.js
+++
@@ -1,9 +0,0 @@
-var events = require('events');
-
-var eventEmitter = new events.EventEmitter();
-
-eventEmitter.on('miyav', function(sayi, sayi2){
-    console.log(sayi +" tane kedinin "+sayi2+" defa miyavladığını duydum sanki!");
-});
-
-eventEmitter.emit('miyav', 2, 3);
